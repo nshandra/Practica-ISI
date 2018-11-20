acf40f490c7edbab412fe58eb244f54499ad496e
nshandra <n.shandra@alumnos.urjc.es>
2018-11-21 00:41:57 +0100

    Add Embotelladora, tests and data driven tests

--- pruebasIDM/Embotelladora.java
+++ pruebasIDM/Embotelladora.java
@@ -1,7 +1,22 @@
-package pruebasIDM;
-
 public class Embotelladora {
-	public int calculaBotellasPequenas(int pequenas, int grandes, int total) {
-		return 0;
+	public static int calculaBotellasPequenas(int pequenas, int grandes, int total) {
+		if (pequenas < 0 || grandes < 0 || total < 0) {
+			return -1;
+		} else {
+			int rest = total - (grandes * 5);
+			if (rest >= 0) {
+				if (pequenas >= rest) {
+					return rest;
+				} else {
+					return -1;
+				}
+			} else {
+				return 0;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.format("%d\n", calculaBotellasPequenas(1,2,6));
 	}
 }
--- pruebasIDM/EmbotelladoraTest.java
+++ pruebasIDM/EmbotelladoraTest.java
@@ -1,18 +1,136 @@
-package pruebasIDM;
-
-import static org.junit.Assert.*;
 import org.junit.*;
-
-import pruebasIDM.Embotelladora;
-
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import static org.junit.Assert.*;
 import java.util.*;
 
-public class BisiestosTest {
+public class EmbotelladoraTest {
 
-	private int year;
-	
-	@Test (expected = NullPointerException.class)
-	public void Embotelladora() {
-		 
+	int pequenas, grandes, total, expected;
+
+	@Test
+	public void testAllZero() {
+		pequenas = 0;
+		grandes = 0;
+		total = 0;
+		expected = 0;
+		assertTrue ("Test All Zero", 
+					expected == Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
 	}
+	
+	@Test
+	public void testGrandesZero() {
+		pequenas = 1;
+		grandes = 0;
+		total = 1;
+		expected = 1;
+		assertTrue ("Test Grandes Zero", 
+					expected == Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+	}
+	
+	@Test
+	public void testPequenasZero() {
+		pequenas = 0;
+		grandes = 1;
+		total = 1;
+		expected = 0;
+		assertTrue ("Test Pequenas Zero", 
+					expected == Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+	}
+	
+	@Test
+	public void testTotalZero() {
+		pequenas = 1;
+		grandes = 1;
+		total = 0;
+		expected = 0;
+		assertTrue ("Test Total Zero", 
+					expected == Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+	}
+	
+	@Test
+	public void testAllPositive() {
+		pequenas = 1;
+		grandes = 1;
+		total = 1;
+		expected = 0;
+		assertTrue ("Test All Positive", 
+					expected == Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+	}
+	
+	@Test
+	public void testGrandesPositive() {
+		pequenas = 0;
+		grandes = 1;
+		total = 1;
+		expected = 0;
+		assertTrue ("Test Grandes Positive", 
+					expected == Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+	}
+	
+	@Test
+	public void testPequenasPositive() {
+		pequenas = 1;
+		grandes = 0;
+		total = 1;
+		expected = 1;
+		assertTrue ("Test Pequenas Positive", 
+					expected == Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+	}
+	
+	@Test
+	public void testTotalPositive() {
+		pequenas = 1;
+		grandes = 1;
+		total = 1;
+		expected = 0;
+		assertTrue ("Test Total Positive", 
+					expected == Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+	}
+	
+//	C7: total > grandes x 5
+	@Test
+	public void testTotalGreaterGrandes() {
+		pequenas = 0;
+		grandes = 1;
+		total = 6;
+		expected = -1;
+		assertTrue ("Test Total Positive", 
+					expected == Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+	}
+	
+//	C8: total = grandes x 5
+	@Test
+	public void testTotalEqualsGrandes() {
+		pequenas = 0;
+		grandes = 1;
+		total = 5;
+		expected = 0;
+		assertTrue ("Test Total Positive", 
+					expected == Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+	}
+	
+//	C9: pequeñas > total
+	@Test
+	public void testPequenasGreaterGrandes() {
+		pequenas = 6;
+		grandes = 0;
+		total = 5;
+		expected = 5;
+		assertTrue ("Test Total Positive", 
+					expected == Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+	}
+	
+//	C10: pequeñas = total
+	@Test
+	public void testPequenasEqualsGrandes() {
+		pequenas = 5;
+		grandes = 0;
+		total = 5;
+		expected = 5;
+		assertTrue ("Test Total Positive", 
+					expected == Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+	}
+	
 }
