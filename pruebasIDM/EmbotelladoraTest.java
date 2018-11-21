e0dcffa739cc504e2dae3c54ae1efecb812307af
nshandra <34254522+nshandra@users.noreply.github.com>
2018-11-21 12:54:52 +0100

    cr-emb.sh update, add AllTestsEmb

--- pruebasIDM/EmbotelladoraTest.java
+++ pruebasIDM/EmbotelladoraTest.java
@@ -1,7 +1,4 @@
 import org.junit.*;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 import static org.junit.Assert.*;
 import java.util.*;
 
--- pruebasIDM/cr-emb.sh
+++ pruebasIDM/cr-emb.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-if [ $# = 1 ]; then
-	java -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar AllTests
-else
-	javac -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar EmbotelladoraTest.java
-	javac -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar AllTests.java
-	java -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar AllTests
-fi
+case $# in
+	0 ) echo '0'
+		javac -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar EmbotelladoraTest.java
+		javac -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar AllTestsEmb.java
+		java -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar AllTestsEmb
+		;;
+	1 ) echo '1'
+		javac -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar DataDrivenEmbotelladoraTest.java
+		javac -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar AllTestsDDEmb.java
+		java -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar AllTestsDDEmb
+		;;
+	2 ) echo '2'
+		javac -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar Embotelladora.java
+		java -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar Embotelladora
+		;;
+	* ) echo '*'
+		exit 1
+		;;
+esac
 exit 0
