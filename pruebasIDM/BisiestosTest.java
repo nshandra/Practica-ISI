e4524ab60875fb3681e034bb8c49ec23aa569832
jaimefdez96 <jfs_x@hotmail.com>
2018-11-23 12:52:46 +0100

    Bisiestos y BisiestosTest definitivos

--- pruebasIDM/BisiestosTest.java
+++ pruebasIDM/BisiestosTest.java
@@ -1,19 +1,53 @@
-package pruebasIDM;
-
 import static org.junit.Assert.*;
 import org.junit.*;
-
-import pruebasIDM.Bisiestos;
-
 import java.util.*;
 
 public class BisiestosTest {
-
+	/* 5 Test:
+	--> 1) Happy path
+	--> 2) Año negativo
+	--> 3) No negativo divisible entre 4 pero no entre 100 ni 400
+	--> 4) No negativo divisible entre 100, pero no entre 4 ni 400
+	--> 5) No negativo divisible entre 4 y 100 pero no entre 400
+	*/
 	private int year;
-	
+
+	//Test 1
+	@Test
+	public void testHappyPath(){
+		year = 2000;
+		assertTrue("Es bisiesto, fallo", true == Bisiestos.esBisiesto(year));
+		System.out.println(year + " es bisiesto");
+	}
+
+	//Test 2
 	@Test (expected = NullPointerException.class)
 	public void testNegativeYear() {
-		year= -700;
-		 
+		year = -2000;
+		Bisiestos.esBisiesto(year);
+	}
+
+	//Test 3
+	@Test
+	public void testDivisibleBy4(){
+		year = 1904;
+		assertTrue("Es bisiesto, fallo", true == Bisiestos.esBisiesto(year));
+		System.out.println(year + " es bisiesto");
+	}
+
+	//Test 4
+	@Test
+	public void testDivisibleBy100(){
+		year = 700;
+		assertTrue("No bisiesto, fallo", false == Bisiestos.esBisiesto(year));
+		System.out.println(year + " no es bisiesto");
+	}
+
+	//Test 5
+	@Test
+	public void testDivisibleBy4And100(){
+		year = 2200;
+		assertTrue("No es bisiesto, fallo", false == Bisiestos.esBisiesto(year));
+		System.out.println(year + " no es bisiesto");
 	}
 }
