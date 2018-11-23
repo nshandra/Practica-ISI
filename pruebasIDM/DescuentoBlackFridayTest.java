bf71dd2a0bec587adec95027bc304aa0749acbc7
jaimefdez96 <jfs_x@hotmail.com>
2018-11-23 12:54:27 +0100

    Descuento y DescuentoTest definitivos

--- pruebasIDM/DescuentoBlackFridayTest.java
+++ pruebasIDM/DescuentoBlackFridayTest.java
@@ -1,18 +1,36 @@
-package pruebasIDM;
-
 import static org.junit.Assert.*;
 import org.junit.*;
-
-import pruebasIDM.DescuentoBlackFriday;
-
 import java.util.*;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
-public class BisiestosTest {
+public class DescuentoBlackFridayTest {
 
-	private int year;
-	
-	@Test (expected = NullPointerException.class)
-	public void test() {
-		 
+	private double price;
+	private Date date = new Date();
+	private LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+	//Test 1
+	@Test
+	public void testCheckPrice(){
+		price = 30.0;
+
+		int month = localDate.getMonthValue();
+		int day   = localDate.getDayOfMonth();
+		double discount = price * (30.0/100.0);
+		if((month == 11) && (day == 23)){
+				assertTrue("Es Black Friday, fallo", discount == DescuentoBlackFriday.PrecioFinal(price));
+		}else{
+				assertTrue("No es Black Friday, fallo", price == DescuentoBlackFriday.PrecioFinal(price));
+		}
+
+	}
+
+	//Test 2
+	@Test (expected = IllegalArgumentException.class)
+	public void testNonPositivePrice(){
+		price = -59.99;
+		DescuentoBlackFriday.PrecioFinal(price);
+
 	}
 }
