65bf98f0f159d63851e947dd709861a9dd9e14f7
nshandra <34254522+nshandra@users.noreply.github.com>
2018-11-25 20:26:06 +0100

    Make subfolders for cases

---
+++ pruebasIDM/Embotelladora/cr-emb.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+case $# in
+	0 ) echo '0'
+		javac -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar EmbotelladoraTest.java
+		javac -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar AllTests.java
+		java -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar AllTests
+		;;
+	1 ) echo '1'
+		javac -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar DataDrivenEmbotelladoraTest.java
+		javac -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar AllTestsDDjava
+		java -cp .:./../hamcrest-core-1.3.jar:./../junit-4.12.jar AllTestsDD
+		;;
+	* ) echo '*'
+		exit 1
+		;;
+esac
+exit 0
