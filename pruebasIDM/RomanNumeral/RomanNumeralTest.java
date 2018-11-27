36238496d89dd2d30c44335dd8b82a30454181a5
Alberto Rafael Rodríguez Iglesias <albertocysec@gmail.com>
2018-11-27 12:29:32 +0100

    Pequeños extras en RomanNumeralTest.java (imprimir resultados explicados)

--- pruebasIDM/RomanNumeral/RomanNumeralTest.java
+++ pruebasIDM/RomanNumeral/RomanNumeralTest.java
@@ -22,24 +22,28 @@ public class RomanNumeralTest {
 	public void testTooManyLetters() {
 		RomanNumeral num = new RomanNumeral();
 		assertTrue("Too many letters", -1 == num.convierte("MMMM") );
+		System.out.println("MMMM has too many letters");
 	}
 
 	@Test
 	public void testInvalidRepeatedLetters(){
 		RomanNumeral num = new RomanNumeral();
 		assertTrue("Invalid repeated letters", -1 == num.convierte("XVVI"));
+		System.out.println("XVVI has invalid repeated letters (VV)");
 	}
 
 	@Test
 	public void testHappyPath(){
 		RomanNumeral num = new RomanNumeral();
 		assertTrue("Happy path", 1467 == num.convierte("MCDLXVII"));
+		System.out.prinln("MCDLXVII es un número correcto y sería: 1467");
 	}
 
 	@Test
 	public void testBadOrder(){
 		RomanNumeral num = new RomanNumeral();
 		assertTrue("Bad order", -1 == num.convierte("MMIM"));
+		System.out.println("MMIM es un número incorrecto porque tiene letras en lugares incorrectos");
 	}
 
 	@Test 
